1f5daed79e942e5d6f0d9947c42df3deabec9344
suryansh raghuwanshi <suryanshraghu10@gmail.com>
2026-05-04 10:38:28 +0530

    Create Day_48.c

---
+++ Day_48.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* left;
+    struct Node* right;
+};
+
+struct Node* newNode(int data) {
+    struct Node* node = (struct Node*)malloc(sizeof(struct Node));
+    node->data = data;
+    node->left = NULL;
+    node->right = NULL;
+    return node;
+}
+
+int countLeaves(struct Node* root) {
+    if (root == NULL)
+        return 0;
+    if (root->left == NULL && root->right == NULL)
+        return 1;
+    return countLeaves(root->left) + countLeaves(root->right);
+}
+
+int main() {
+    struct Node* root = newNode(1);
+    root->left = newNode(2);
+    root->right = newNode(3);
+    root->left->left = newNode(4);
+    root->left->right = newNode(5);
+
+    printf("%d", countLeaves(root));
+    return 0;
+}
