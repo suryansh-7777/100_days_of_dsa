0d07b0f262710e8170a61dd8d0c311841f931bb6
suryansh raghuwanshi <suryanshraghu10@gmail.com>
2026-05-04 11:14:05 +0530

    Create Days81.c

---
+++ Days81.c
@@ -0,0 +1,9 @@
+n = int(input())
+arr = list(map(int, input().split()))
+
+for i in range(n):
+    for j in range(0, n - i - 1):
+        if arr[j] > arr[j + 1]:
+            arr[j], arr[j + 1] = arr[j + 1], arr[j]
+
+print(*arr)
