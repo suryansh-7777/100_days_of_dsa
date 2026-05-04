c354359a4a40a0e0ce23b73389992a78c53a69e6
suryansh raghuwanshi <suryanshraghu10@gmail.com>
2026-05-04 11:14:57 +0530

    Create Days83.c

---
+++ Days83.c
@@ -0,0 +1,11 @@
+n = int(input())
+arr = list(map(int, input().split()))
+
+for i in range(n):
+    min_idx = i
+    for j in range(i + 1, n):
+        if arr[j] < arr[min_idx]:
+            min_idx = j
+    arr[i], arr[min_idx] = arr[min_idx], arr[i]
+
+print(*arr)
