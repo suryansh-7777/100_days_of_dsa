98a59af5fbfc74c943f9daa53e077d1b2263eedb
suryansh raghuwanshi <suryanshraghu10@gmail.com>
2026-05-04 10:39:39 +0530

    Create Day_51.c

---
+++ Day_51.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* left;
+    struct Node* right;
+};
+
+struct Node* createNode(int data) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->left = newNode->right = NULL;
+    return newNode;
+}
+
+struct Node* insert(struct Node* root, int data) {
+    if (root == NULL) return createNode(data);
+    if (data < root->data)
+        root->left = insert(root->left, data);
+    else
+        root->right = insert(root->right, data);
+    return root;
+}
+
+struct Node* findLCA(struct Node* root, int n1, int n2) {
+    while (root != NULL) {
+        if (n1 < root->data && n2 < root->data)
+            root = root->left;
+        else if (n1 > root->data && n2 > root->data)
+            root = root->right;
+        else
+            return root;
+    }
+    return NULL;
+}
+
+int main() {
+    int n, x, n1, n2;
+    struct Node* root = NULL;
+
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &x);
+        root = insert(root, x);
+    }
+
+    scanf("%d %d", &n1, &n2);
+
+    struct Node* lca = findLCA(root, n1, n2);
+    if (lca != NULL)
+        printf("%d\n", lca->data);
+
+    return 0;
+}
