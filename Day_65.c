41e6a52e9c1a3228c8a980e54ee741e74091dc2a
suryansh raghuwanshi <suryanshraghu10@gmail.com>
2026-05-04 11:05:15 +0530

    Create Day_65.c

---
+++ Day_65.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#define MAX 100
+
+int visited[MAX];
+
+int dfs(int adj[MAX][MAX], int n, int u, int parent) {
+    visited[u] = 1;
+    for (int v = 0; v < n; v++) {
+        if (adj[u][v]) {
+            if (!visited[v]) {
+                if (dfs(adj, n, v, u)) return 1;
+            } else if (v != parent) {
+                return 1;
+            }
+        }
+    }
+    return 0;
+}
+
+int hasCycle(int adj[MAX][MAX], int n) {
+    for (int i = 0; i < n; i++) visited[i] = 0;
+    for (int i = 0; i < n; i++) {
+        if (!visited[i]) {
+            if (dfs(adj, n, i, -1)) return 1;
+        }
+    }
+    return 0;
+}
+
+int main() {
+    int n, adj[MAX][MAX];
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++)
+            scanf("%d", &adj[i][j]);
+    if (hasCycle(adj, n)) printf("YES");
+    else printf("NO");
+    return 0;
+}
