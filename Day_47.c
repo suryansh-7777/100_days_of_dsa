6e45b0e1833a52de3d13d270e33aa49e0b4059de
suryansh raghuwanshi <suryanshraghu10@gmail.com>
2026-05-04 10:37:52 +0530

    Create Day_47.c

---
+++ Day_47.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* left;
+    struct Node* right;
+};
+
+struct Node* newNode(int data) {
+    struct Node* node = (struct Node*)malloc(sizeof(struct Node));
+    node->data = data;
+    node->left = NULL;
+    node->right = NULL;
+    return node;
+}
+
+int height(struct Node* root) {
+    if (root == NULL)
+        return 0;
+    int left = height(root->left);
+    int right = height(root->right);
+    return 1 + (left > right ? left : right);
+}
+
+int main() {
+    struct Node* root = newNode(1);
+    root->left = newNode(2);
+    root->right = newNode(3);
+    root->left->left = newNode(4);
+    root->left->right = newNode(5);
+
+    printf("%d", height(root));
+    return 0;
+}
